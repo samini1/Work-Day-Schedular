25dc1590e622665b09a586a572b3727271469b90
samini1 <92944248+samini1@users.noreply.github.com>
2021-12-11 09:04:31 -0600

    working on local storage arrangements

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,42 +5,57 @@ const currentHour = moment().format("H");
 const hours = ['9','10','11','12','1','2','3','4','5'];
 const worldTime = ['9','10','11','12','13','14','15','16','17'];
 const period = ['AM','AM','AM','PM','PM','PM','PM','PM','PM'];
-
-
+var entries ={};
 
 //Current day should be displayed at the top of the calendar
 currentDay.text(now);
 
 //Time blocks for standard business hours propogated below
-$.each(hours, function(i, val) {
-    var blockHourSlot = $(
-    '<div class = "hour">txt</div>'
-    );
-    
-    // console.log(worldTime[i]);
-    if (currentHour > worldTime[i]) {
-    var blockElEntry=  $(
-        '<div id = "time-block" class = "past row ">Enter event</div>'
-    );
-    } else if (currentHour === worldTime[i])
-    {  var blockElEntry=  $(
-        '<div id = "time-block" class = "present row ">Enter event</div>'
-    );
+var createEntries = function(entryText){
+  $.each(hours, function (i, val) {
+    var blockHourSlot = $('<div class = "hour"></div>');
+      blockHourSlot.text(val + period[i]);
+      blockEl.append(blockHourSlot);
+  
+    // need to parse strings into number from array to make sure this works
+      if (parseInt(currentHour) > worldTime[i]) {
+      var blockElEntry = $(
+        '<div class = "time-block past row time-block-item"><p id="entry-display"></p></div>')
+        .text(entryText);
+      blockEl.append(blockElEntry);
+    } else if (parseInt(currentHour) == worldTime[i]) {
+      var blockElEntry = $(
+        '<div class = "time-block present row time-block-item"<p id="entry-display"></p></div>')
+        .text(entryText);
+      blockEl.append(blockElEntry);
     } else {
-        var blockElEntry = $('<div id = "time-block" class = "future row">Enter event</div>'
-
-        )
+      var blockElEntry = $(
+        '<div  class = "time-block future row time-block-item"><p id="entry-display"></p></div>')
+        .text(entryText);
+      blockEl.append(blockElEntry);
     }
     
-    var saveButton = $(
-        '<button class ="saveBtn"><i>Save</i></button>'
-    );    
-    blockHourSlot.text(val + period[i]);
-    blockEl.append(blockHourSlot);
-    blockEl.append(blockElEntry);
-    blockEl.append(saveButton);
 
-});
+    var saveButton = $('<button class ="saveBtn"><i>Save</i></button>');  
+    blockEl.append(saveButton);
+  });
+}
+
+// push text values for the new entries
+var loadEntries = function() {
+  if (!entries) {
+    entries = {
+    textValue: []
+    };
+  }
+
+  $.each(entries, function(list, arr) {
+    arr.forEach(function(entry) {
+      createEntry(entry.text)
+    })
+  })
+}
+
 
 // createTimeBlock;
 
@@ -48,15 +63,59 @@ $.each(hours, function(i, val) {
 
 //click into time block and enter an event
 
-function editEntry(){
-    console.log('clicked entry')
-}
+function editEntry(event){
+    console.log(event.target);
+    var textEntry = $(this)
+    .text()
+    .trim();
 
-$("#time-block").click(editEntry);
+    //create text area 
+    var textInput = $("<textarea>").val(textEntry);
+    $(this).replaceWith(textInput);
+
+    //focus textarea
+    textInput.trigger("focus");
+};
+
+//process to save and reinstate as p
+$(".time-block").on("blur", "textarea", function() {
+  //get textarea value
+  var textEntry = $(this).val();  
+
+//part where p element is created and reinstated
+var entryP= $("<p>")  
+  .text(textEntry);
+
+  entryP.attr("id", "entry-display")
+$(this).replaceWith(entryP);
+
+});
+
+$(".time-block").on("click", "p", editEntry);
 
 //save button for time block saves text in local storage
 function saveEntry() {
+  var displayText = 
+  document.getElementById("entry-display").value;
+  console.log(document.getElementById("entry-display").value);
+  
+  
+  localStorage.setItem('entries', JSON.stringify(entries));
     console.log("clicked save");
-}
+};
 
-$(".saveBtn").click(saveEntry);
+//update entry arrary when button pushed
+$(".saveBtn").click(function() {
+  var entryText= $("#entry-display").val();
+
+  createEntry(entryText);
+
+  entries.push({
+    textValue: entryText
+  });
+
+  saveEntry();
+});
+
+var displayText = localStorage.getItem("displayText");
+  document.getElementById('entry-display').value = displayText;
