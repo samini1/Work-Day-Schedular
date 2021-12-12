32345a191d94c369ef976f163019a85577becee2
samini1 <92944248+samini1@users.noreply.github.com>
2021-12-12 13:21:35 -0600

    reverting to previous format. Still need to figure out methods to save into local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,100 +2,98 @@ var currentDay = $("#currentDay");
 var blockEl = $("#block-container");
 const now = moment().format("dddd, MMMM Do");
 const currentHour = moment().format("H");
-const hours = ['9','10','11','12','1','2','3','4','5'];
-const worldTime = ['9','10','11','12','13','14','15','16','17'];
-const period = ['AM','AM','AM','PM','PM','PM','PM','PM','PM'];
+const hours = ["9", "10", "11", "12", "1", "2", "3", "4", "5"];
+const worldTime = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
+const period = ["AM", "AM", "AM", "PM", "PM", "PM", "PM", "PM", "PM"];
 var entries = {};
 //Current day should be displayed at the top of the calendar
 currentDay.text(now);
 
-// //Time blocks classes propagated below
-// var createEntries = function(){
-// $.each(hours, function (i, val) {
-//   var blockHourSlot = $('<div class = "hour">txt</div>');
-//     blockHourSlot.text(val + period[i]);
-//     blockEl.append(blockHourSlot);
- 
-//   // need to parse strings into number from array to make sure this works
-//     if (parseInt(currentHour) > worldTime[i]) {
-//     var blockElEntry = $(
-//       '<div class = "time-block past row time-block-item"><p id="entry-display"> Enter event </p></div>'
-//     );
-//     blockEl.append(blockElEntry);
-//   } else if (parseInt(currentHour) == worldTime[i]) {
-//     var blockElEntry = $(
-//       '<div class = "time-block present row time-block-item"> <p id="entry-display"> Enter event </p></div>'
-//     );
-//     blockEl.append(blockElEntry);
-//   } else {
-//     var blockElEntry = $(
-//       '<div  class = "time-block future row time-block-item"><p id="entry-display">Enter event</p></div>'
-//     );
-//     blockEl.append(blockElEntry);
-//   }
-  
+//Time blocks for standard business hours propagated below
+var createEntries = function(){
+$.each(hours, function (i, val) {
+  var blockHourSlot = $('<div class = "hour">txt</div>');
+    blockHourSlot.text(val + period[i]);
+    blockEl.append(blockHourSlot);
 
-//   var saveButton = $('<button class ="saveBtn"><i>Save</i></button>');  
-//   blockEl.append(saveButton);
-// });
-// }
+  // need to parse strings into number from array to make sure this works
+    if (parseInt(currentHour) > worldTime[i]) {
+    var blockElEntry = $(
+      '<div class = "time-block past row time-block-item"><p id="entry-display"> Enter event </p></div>'
+    );
+    blockEl.append(blockElEntry);
+  } else if (parseInt(currentHour) == worldTime[i]) {
+    var blockElEntry = $(
+      '<div class = "time-block present row time-block-item"> <p id="entry-display"> Enter event </p></div>'
+    );
+    blockEl.append(blockElEntry);
+  } else {
+    var blockElEntry = $(
+      '<div  class = "time-block future row time-block-item"><p id="entry-display">Enter event</p></div>'
+    );
+    blockEl.append(blockElEntry);
+  }
 
-// // push text values for the new entries
-// var loadEntries = function() {
-//   if (!entries) {
-//     entries = {
-//       textValue: ["Enter"]
-//     };
-//   }
+  var saveButton = $('<button class ="saveBtn"><i>Save</i></button>');
+  blockEl.append(saveButton);
+});
+}
 
-//   createEntries();
-// }
+// push text values for the new entries
+var loadEntries = function() {
+  if (!entries) {
+    entries = {
+      textValue: ["Enter"]
+    };
+  }
 
+  createEntries();
+}
 
-// // createTimeBlock;
+// createTimeBlock;
 
-// //time blocks color-coded for past, present, or future
+//time blocks color-coded for past, present, or future
 
-// //click into time block and enter an event
+//click into time block and enter an event
 
-// function editEntry(event){    
-//     var textEntry = $(this)
-//     .text()
-//     .trim();
+function editEntry(event){
+    var textEntry = $(this)
+    .text()
+    .trim();
 
-//     //create text area 
-//     var textInput = $("<textarea>").val(textEntry);
-//     $(this).replaceWith(textInput);
+    //create text area
+    var textInput = $("<textarea>").val(textEntry);
+    $(this).replaceWith(textInput);
 
-//     //focus textarea
-//     textInput.trigger("focus");
-// };
+    //focus textarea
+    textInput.trigger("focus");
+};
 
-// //process to save and reinstate as p
-// $("#block-container").on("blur", "textarea", function() {
-//   //get textarea value
-//   var textEntry = $(this).val();  
+//process to save and reinstate as p
+$("#block-container").on("blur", "textarea", function() {
+  //get textarea value
+  var textEntry = $(this).val();
 
-// //part where p element is created and reinstated
-// var entryP= $("<p>")  
-//   .text(textEntry);
+//part where p element is created and reinstated
+var entryP= $("<p>")
+  .text(textEntry);
 
-//   entryP.attr("id", "entry-display")
-// $(this).replaceWith(entryP);
+  entryP.attr("id", "entry-display")
+$(this).replaceWith(entryP);
 
-// });
+});
 
-// $("#block-container").on("click", "p", editEntry);
+$("#block-container").on("click", "p", editEntry);
 
-// //save button for time block saves text in local storage
-// function saveEntry(event) {
-//   var entryText = this.previousSibling.textContent
-//   console.log(this.previousSibling.textContent)
-//   console.log(entries.textValue);
-  
-//     localStorage.setItem('entries', JSON.stringify(entries));
-//     console.log("clicked save");
-// };
+//save button for time block saves text in local storage
+function saveEntry(event) {
+  var entryText = this.previousSibling.textContent
+  console.log(this.previousSibling.textContent)
+  console.log(entries.textValue);
 
-// $("#block-container").on("click", ".saveBtn", saveEntry);
-// loadEntries();
+    localStorage.setItem('entries', JSON.stringify(entries));
+    console.log("clicked save");
+};
+
+$("#block-container").on("click", ".saveBtn", saveEntry);
+loadEntries();
--- assets/js/scriptpre.js
+++ assets/js/scriptpre.js
@@ -9,91 +9,91 @@ var entries = {};
 //Current day should be displayed at the top of the calendar
 currentDay.text(now);
 
-// //Time blocks for standard business hours propagated below
-// var createEntries = function(){
-// $.each(hours, function (i, val) {
-//   var blockHourSlot = $('<div class = "hour">txt</div>');
-//     blockHourSlot.text(val + period[i]);
-//     blockEl.append(blockHourSlot);
+//Time blocks for standard business hours propagated below
+var createEntries = function(){
+$.each(hours, function (i, val) {
+  var blockHourSlot = $('<div class = "hour">txt</div>');
+    blockHourSlot.text(val + period[i]);
+    blockEl.append(blockHourSlot);
 
-//   // need to parse strings into number from array to make sure this works
-//     if (parseInt(currentHour) > worldTime[i]) {
-//     var blockElEntry = $(
-//       '<div class = "time-block past row time-block-item"><p id="entry-display"> Enter event </p></div>'
-//     );
-//     blockEl.append(blockElEntry);
-//   } else if (parseInt(currentHour) == worldTime[i]) {
-//     var blockElEntry = $(
-//       '<div class = "time-block present row time-block-item"> <p id="entry-display"> Enter event </p></div>'
-//     );
-//     blockEl.append(blockElEntry);
-//   } else {
-//     var blockElEntry = $(
-//       '<div  class = "time-block future row time-block-item"><p id="entry-display">Enter event</p></div>'
-//     );
-//     blockEl.append(blockElEntry);
-//   }
+  // need to parse strings into number from array to make sure this works
+    if (parseInt(currentHour) > worldTime[i]) {
+    var blockElEntry = $(
+      '<div class = "time-block past row time-block-item"><p id="entry-display"> Enter event </p></div>'
+    );
+    blockEl.append(blockElEntry);
+  } else if (parseInt(currentHour) == worldTime[i]) {
+    var blockElEntry = $(
+      '<div class = "time-block present row time-block-item"> <p id="entry-display"> Enter event </p></div>'
+    );
+    blockEl.append(blockElEntry);
+  } else {
+    var blockElEntry = $(
+      '<div  class = "time-block future row time-block-item"><p id="entry-display">Enter event</p></div>'
+    );
+    blockEl.append(blockElEntry);
+  }
 
-//   var saveButton = $('<button class ="saveBtn"><i>Save</i></button>');
-//   blockEl.append(saveButton);
-// });
-// }
+  var saveButton = $('<button class ="saveBtn"><i>Save</i></button>');
+  blockEl.append(saveButton);
+});
+}
 
-// // push text values for the new entries
-// var loadEntries = function() {
-//   if (!entries) {
-//     entries = {
-//       textValue: ["Enter"]
-//     };
-//   }
+// push text values for the new entries
+var loadEntries = function() {
+  if (!entries) {
+    entries = {
+      textValue: ["Enter"]
+    };
+  }
 
-//   createEntries();
-// }
+  createEntries();
+}
 
-// // createTimeBlock;
+// createTimeBlock;
 
-// //time blocks color-coded for past, present, or future
+//time blocks color-coded for past, present, or future
 
-// //click into time block and enter an event
+//click into time block and enter an event
 
-// function editEntry(event){
-//     var textEntry = $(this)
-//     .text()
-//     .trim();
+function editEntry(event){
+    var textEntry = $(this)
+    .text()
+    .trim();
 
-//     //create text area
-//     var textInput = $("<textarea>").val(textEntry);
-//     $(this).replaceWith(textInput);
+    //create text area
+    var textInput = $("<textarea>").val(textEntry);
+    $(this).replaceWith(textInput);
 
-//     //focus textarea
-//     textInput.trigger("focus");
-// };
+    //focus textarea
+    textInput.trigger("focus");
+};
 
-// //process to save and reinstate as p
-// $("#block-container").on("blur", "textarea", function() {
-//   //get textarea value
-//   var textEntry = $(this).val();
+//process to save and reinstate as p
+$("#block-container").on("blur", "textarea", function() {
+  //get textarea value
+  var textEntry = $(this).val();
 
-// //part where p element is created and reinstated
-// var entryP= $("<p>")
-//   .text(textEntry);
+//part where p element is created and reinstated
+var entryP= $("<p>")
+  .text(textEntry);
 
-//   entryP.attr("id", "entry-display")
-// $(this).replaceWith(entryP);
+  entryP.attr("id", "entry-display")
+$(this).replaceWith(entryP);
 
-// });
+});
 
-// $("#block-container").on("click", "p", editEntry);
+$("#block-container").on("click", "p", editEntry);
 
-// //save button for time block saves text in local storage
-// function saveEntry(event) {
-//   var entryText = this.previousSibling.textContent
-//   console.log(this.previousSibling.textContent)
-//   console.log(entries.textValue);
+//save button for time block saves text in local storage
+function saveEntry(event) {
+  var entryText = this.previousSibling.textContent
+  console.log(this.previousSibling.textContent)
+  console.log(entries.textValue);
 
-//     localStorage.setItem('entries', JSON.stringify(entries));
-//     console.log("clicked save");
-// };
+    localStorage.setItem('entries', JSON.stringify(entries));
+    console.log("clicked save");
+};
 
-// $("#block-container").on("click", ".saveBtn", saveEntry);
-// loadEntries();
+$("#block-container").on("click", ".saveBtn", saveEntry);
+loadEntries();
